ba0d6ce6a3830564a6147b5bf9d1fa0e333cc1ca
Md Mahibul Hasan <hyde.sohag@gmail.com>
2025-06-08 16:55:41 -0400

    List User Story Done

--- service/routes.py
+++ service/routes.py
@@ -82,7 +82,7 @@ def get_accounts(account_id):
     """
     app.logger.info("Request to get an Account")
 
-    account = Account().find(account_id)
+    account = Account.find(account_id)
 
     if account is None:
         abort(status.HTTP_404_NOT_FOUND, f"Account with {account_id} not available")
@@ -100,8 +100,19 @@ def get_accounts(account_id):
 ######################################################################
 # UPDATE AN EXISTING ACCOUNT
 ######################################################################
+@app.route("/accounts/<int:account_id>", methods=["PUT"])
+def update_accounts(account_id):
+    account = Account.find(account_id)
+    if account is None:
+        abort(status.HTTP_404_NOT_FOUND, f"Account with {account_id} not available")
+    account.deserialize(request.get_json()) #parse data from PUT requests
+    account.update()
 
-# ... place you code here to UPDATE an account ...
+    location_url = f"/accounts/{account_id}"  # Remove once get_accounts has been implemented
+    return make_response(
+        jsonify(account.serialize()), status.HTTP_200_OK, {"Location": location_url}
+    )
+
 
 
 ######################################################################
